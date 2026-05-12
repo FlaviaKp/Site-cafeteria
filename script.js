416950158269e1a59ff3f1ac12639bc9daaca663
FlaviaKp <flavia.ka@hotmail.com>
2026-05-11 22:20:12 -0300

    add menu

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 // Dados dos produtos
 const produtos = {
     quentes: [
-        { nome: "Cappuccino", descricao: "Espresso com leite vaporizado e espuma cremosa, polvilhado com canela (270ml).", preco: "R$ 12,00", imagem: "imagens/bebidas quentes/cap_italiano.jpg" }
+        { nome: "Cappuccino", descricao: "Espresso com leite vaporizado e espuma cremosa, polvilhado com canela (270ml).", preco: "R$ 12,00", imagem: "imagens/cardapio/bebidas quentes/cap_italiano.jpg" }
 
-        ,{nome: "Big Latte", descricao: "Uma dose dupla de café espresso (Orfeu) e leite vaporizado cremosinho. 500ml de café com leite para você se esbaldar!.", preco: "R$ 23,90", imagem: "imagens/bebidas quentes/big_latte.jpg" }
+        ,{nome: "Big Latte", descricao: "Uma dose dupla de café espresso (Orfeu) e leite vaporizado cremosinho. 500ml de café com leite para você se esbaldar!.", preco: "R$ 23,90", imagem: "imagens/cardapio/bebidas quentes/big_latte.jpg" }
 
-        ,{nome: "Cappuccino Especial", descricao: "Sua composição leva calda de chocolate gourmet meio amargo, uma mistura deliciosa do nosso cappuccino especial, leite vaporizado, uma pitadinha de canela em pó e, opcionalmente, chantilly em um potinho enviado separadamente (270ml).", preco: "R$ 19,90", imagem: "imagens/bebidas quentes/cap_especial.jpg" }
+        ,{nome: "Cappuccino Especial", descricao: "Sua composição leva calda de chocolate gourmet meio amargo, uma mistura deliciosa do nosso cappuccino especial, leite vaporizado, uma pitadinha de canela em pó e chantilly em um potinho enviado separadamente (opcional) (270ml).", preco: "R$ 19,90", imagem: "imagens/cardapio/bebidas quentes/cap_especial.jpg" }
+
+        ,{nome: "Chocolate Quente Europeu", descricao: "Esse chocolate quente é muito consumido no friozinho europeu. Sua composição leva um blend especial de chocolate em pó, leite vaporizado bem cremoso e chantilly em um potinho enviado separadamente (opcional) (270ml).", preco: "R$ 24,00", imagem: "imagens/cardapio/bebidas quentes/chocolate_quente.jpg" }
     ],
     geladas: [
-        { nome: "Coffee", descricao: "Café gelado com leite condensado e pedras de gelo crocantes.", preco: "R$ 13,00", imagem: "imagens/bebidas geladas/frappês.jpg" },
-        { nome: "Frappuccino", descricao: "Blend cremoso de café, leite e chantilly batido com gelo.", preco: "R$ 16,00", imagem: "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?w=400&h=400&fit=crop&crop=face" }
+        { nome: "Iced Latte", descricao: "Um shot de café espresso, leite e gelo (baunilha opcional). Simples e delicioso! (400ml)", preco: "R$ 17,90", imagem: "imagens/cardapio/bebidas geladas/iced_latte.jpg" },
+
+        ,{ nome: "Smoothies", descricao: "Beeem gelado e saboroso, nosso smoothhie é perfeito para aqueles dias em que Curitiba entra no modo verão (500ml).", preco: "R$ 24,90", imagem: "imagens/cardapio/bebidas geladas/smoothies.jpg" }
+
+        ,{ nome: "Cappuccino Gelado", descricao: "Delicioso blend do nosso cappuccino especial, calda de chocolate meio amargo, leite, cubos de gelo, chantilly e canela em pó... Essa bebida tem o incrível poder de melhorar o seu dia!", preco: "R$ 24,90", imagem: "imagens/cardapio/bebidas geladas/cap_gelado.jpg" }
+
+        ,{ nome: "Frappês", descricao: "Bebida cremosinhha, bem gelada e deliciosa! perfeita para uma pausa no meio do dia. Ele leva café expresso (opcional), leite, chantilly e o ingrediente do sabor escolhido (500ml).", preco: "R$ 24,90", imagem: "imagens/cardapio/bebidas geladas/frappes.jpg" }
     ],
     bolo_pote: [
-        { nome: "Ninho com Nutella", descricao: "Camadas cremosas de leite ninho com a legítima Nutella.", preco: "R$ 15,00", imagem: "https://images.unsplash.com/photo-1587314168485-3236d6710814?w=400&h=400&fit=crop" },
-        { nome: "Red Velvet", descricao: "Massa aveludada com recheio de cream cheese e geleia de morango.", preco: "R$ 16,00", imagem: "https://images.unsplash.com/photo-1616031037011-087000171abe?w=400&h=400&fit=crop" }
+        { nome: "Bolo de Pote de Cenoura com Brigadeiro", descricao: "A combinação perfeita de um bolo de cenoura fofinho com brigadeiro cremoso dentro de um potinho! (200ml).", preco: "R$ 19,90", imagem: "imagens/cardapio/bolo no pote/cenoura.jpg" }
+
+        ,{ nome: "Bolo de Pote de Brigadeiro Dois Amores", descricao: "Saboreie a felicidade em cada camada com o nosso Bolo de Pote de Dois Amores. É uma explosão de brigadeiros preto e branco em cada colherada. (220ml)", preco: "R$ 22,00", imagem: "imagens/cardapio/bolo no pote/dois_amores.jpg" }
+
+        ,{ nome: "Bolo de Pote de Brigadeiro e Caramelo Salgado", descricao: "Feito com camadas de bolo de chocolate super macio, brigadeiro cremoso artesanal e um toque surpreendente de caramelo salgado (220ml).", preco: "R$ 19,90", imagem: "imagens/cardapio/bolo no pote/caramelo.jpg" }
     ],
     bombom_pote: [
-        { nome: "Bombom de Morango", descricao: "Morangos frescos, brigadeiro branco e cobertura de chocolate meio amargo.", preco: "R$ 14,00", imagem: "https://images.unsplash.com/photo-1603532648955-039310d9ed75?w=400&h=400&fit=crop" },
-        { nome: "Bombom de Uva", descricao: "Uvas selecionadas com creme belga e ganache de chocolate ao leite.", preco: "R$ 14,00", imagem: "https://images.unsplash.com/photo-1582293041079-7814c2f12063?w=400&h=400&fit=crop" }
+        { nome: "Bombom de Pote: Dois Amores com Uva", descricao: "Brigadeiro preto 50% cacau com brigadeiro branco bem cremosinho e uas verdes sem semente! (120ml)", preco: "R$ 17,99", imagem: "imagens/cardapio/bombom no pote/dois_amores.jpg" }
+
+        ,{ nome: "Bombom de Pote: Mousse de Maracujá com Creme de Alpino", descricao: "A combinação perfeita do azedinhho do maracujá com o sabor marcante do chocolate Alpino! (120ml).", preco: "R$ 17,99", imagem: "imagens/cardapio/bombom no pote/musse_maracuja.jpg" }
     ],
     brownies: [
-        { nome: "Brownie Tradicional", descricao: "Brownie super chocolatudo com casquinha crocante e interior úmido.", preco: "R$ 10,00", imagem: "https://images.unsplash.com/photo-1461023058943-07fcbe16d735?w=400&h=400&fit=crop" },
-        { nome: "Brownie com Sorvete", descricao: "Nosso brownie quente servido com uma bola de sorvete de baunilha.", preco: "R$ 18,00", imagem: "https://images.unsplash.com/photo-1564355808539-22fda35bed7e?w=400&h=400&fit=crop" }
+        { nome: "Brownie Tradicional", descricao: "Brownie super chocolatudo com casquinha crocante e interior úmido.", preco: "R$ 10,00", imagem: "https://images.unsplash.com/photo-1461023058943-07fcbe16d735?w=400&h=400&fit=crop" }
+        
+        ,{ nome: "Brownie com Sorvete", descricao: "Nosso brownie quente servido com uma bola de sorvete de baunilha.", preco: "R$ 18,00", imagem: "https://images.unsplash.com/photo-1564355808539-22fda35bed7e?w=400&h=400&fit=crop" }
     ],
     tortas: [
         { nome: "Banoffee", descricao: "Torta de banana com doce de leite caseiro e chantilly levemente polvilhado.", preco: "R$ 14,00", imagem: "https://images.unsplash.com/photo-1535927842660-3487d4a62e02?w=400&h=400&fit=crop" },
